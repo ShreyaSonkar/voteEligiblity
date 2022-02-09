22b6041fde170eb295ad1f73e5c01e0c8935a978
ShreyaSonker <98208793+ShreyaSonkar@users.noreply.github.com>
2022-02-09 12:26:33 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int age;
+	cin >> age;
+	
+	if(age >= 18) {
+		cout << "Person is eligible";
+	}
+	else {
+		cout << "not eligible";
+	}
+	
+	return 0;
+		
+}
